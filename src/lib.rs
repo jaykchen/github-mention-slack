59f2cb59369d708c5f163afced680ffb8ca8aa70
Ji Chen <jaykchen@gmail.com>
2023-05-10 22:09:44 -0400

    add airtable

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,16 @@
+use airtable_flows::create_record;
+use chrono::DateTime;
 use dotenv::dotenv;
 use github_flows::{
     listen_to_event,
+    octocrab::models::{
+        events::payload::{IssueCommentEventAction, IssuesEventAction, PullRequestEventAction},
+    },
     EventPayload,
     GithubLogin::Provided,
-    octocrab::models::events::payload::{
-        IssueCommentEventAction, IssuesEventAction, PullRequestEventAction,
-    },
 };
 use slack_flows::send_message_to_channel;
 use std::env;
-
 #[no_mangle]
 #[tokio::main(flavor = "current_thread")]
 pub async fn run() -> anyhow::Result<()> {
@@ -40,20 +41,28 @@ async fn handler(owner: &str, payload: EventPayload) {
     let slack_workspace = env::var("slack_workspace").unwrap_or("secondstate".to_string());
     let slack_channel = env::var("slack_channel").unwrap_or("github-status".to_string());
 
+    let airtable_token_name = env::var("airtable_token_name").unwrap_or("github".to_string());
+    let airtable_base_id = env::var("airtable_base_id").unwrap_or("appNEswczILgUsxML".to_string());
+    let airtable_table_name = env::var("airtable_table_name").unwrap_or("fork".to_string());
+
     let at_string = format!("@{}", owner);
     let mut is_mentioned = false;
+    let mut name = "".to_string();
+    let mut time = DateTime::default();
     let mut title = "".to_string();
     let mut html_url = "".to_string();
 
     match payload {
         EventPayload::IssuesEvent(e) => {
             let issue = e.issue;
-            let action = e.action; 
+            let action = e.action;
             is_mentioned = issue.body.unwrap_or("".to_string()).contains(&at_string);
 
             if is_mentioned && action != IssuesEventAction::Closed {
+                name = issue.user.login;
                 title = issue.title;
                 html_url = issue.html_url.to_string();
+                time = issue.created_at;
             }
         }
 
@@ -63,8 +72,10 @@ async fn handler(owner: &str, payload: EventPayload) {
             is_mentioned = comment.body.unwrap_or("".to_string()).contains(&at_string);
 
             if is_mentioned && action != IssueCommentEventAction::Deleted {
+                name = comment.user.login;
                 title = e.issue.title;
                 html_url = comment.html_url.to_string();
+                time = comment.created_at;
             }
         }
 
@@ -76,11 +87,13 @@ async fn handler(owner: &str, payload: EventPayload) {
                 .unwrap_or("".to_string())
                 .contains(&at_string);
             if is_mentioned && action != PullRequestEventAction::Closed {
+                name = pull_request.user.unwrap().login;
                 title = pull_request.title.unwrap();
                 html_url = pull_request
                     .html_url
                     .expect("html_url not found")
                     .to_string();
+                time = pull_request.created_at.unwrap();
             }
         }
 
@@ -88,8 +101,10 @@ async fn handler(owner: &str, payload: EventPayload) {
             let review = e.review;
             is_mentioned = review.body.unwrap_or("".to_string()).contains(&at_string);
             if is_mentioned {
+                name = review.user.unwrap().login;
                 title = e.pull_request.title.unwrap();
                 html_url = review.html_url.to_string();
+                time = review.submitted_at.unwrap();
             }
         }
 
@@ -98,8 +113,10 @@ async fn handler(owner: &str, payload: EventPayload) {
             is_mentioned = comment.body.unwrap_or("".to_string()).contains(&at_string);
 
             if is_mentioned {
+                name = comment.user.login;
                 title = e.pull_request.title.unwrap();
                 html_url = comment.html_url.to_string();
+                time = comment.created_at;
             }
         }
 
@@ -107,7 +124,19 @@ async fn handler(owner: &str, payload: EventPayload) {
     }
 
     if is_mentioned {
-        let text = format!("You are mentioned in:\n{title}\n{html_url}");
+        let text = format!("{name} mentioned you in {title}\n{html_url}");
         send_message_to_channel(&slack_workspace, &slack_channel, text);
+
+        let data = serde_json::json!({
+        "Name": name,
+        "Repo": html_url,
+        "Created": time,
+        });
+        create_record(
+            &airtable_token_name,
+            &airtable_base_id,
+            &airtable_table_name,
+            data,
+        )
     }
 }
